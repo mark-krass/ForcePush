2ad0b0aaa3e2d8c7a6c908b893de25a6d22b7929
degra.dance <goto97@mail.ru>
2018-12-01 18:11:33 +0300

    легаси код

---
+++ src/Accident.java
@@ -0,0 +1,5 @@
+public class Accident {
+
+    String name;
+    String detectorID; //здесь будет нечто для определения детектора
+}
---
+++ src/Event.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+import java.util.List;
+
+public class Event {
+
+    String eventName;
+
+    Date dateFrom;
+    Date dateTo;
+
+    String cameraID;
+
+    List<Accident> accidents;
+}
